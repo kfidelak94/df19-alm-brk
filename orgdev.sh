e2758fe2fa285c3f0aa2de1e62800ee96a51b707
kfidelak94 <31737796+kfidelak94@users.noreply.github.com>
2019-11-10 23:22:40 -0700

    Add files via upload

--- orgdev.sh
+++ orgdev.sh
@@ -13,6 +13,7 @@ hub delete -y $gitusername/dfdemo > /dev/null 2>&1
 node ./testing.js
 
 # Demo script
+# Create project and initialize Git and create remote repository
 dprintf "mkdir dfdemo"
 dprintf "cd dfdemo"
 dprintf "git init"
@@ -27,7 +28,7 @@ git push origin -d feature1 > /dev/null 2>&1
 git push origin -d uat > /dev/null 2>&1
 git checkout -b uat > /dev/null 2>&1
 git push origin uat > /dev/null 2>&1
-
+# Do development in Sandbox and pull changes and push them to source control repo
 dprintf "sfdx force:source:pull -u $devsb" 
 echo '*******Now we will make a change in the Sandbox'
 dprintf "sfdx force:org:open -u $devsb"
@@ -42,12 +43,14 @@ echo '*******Now we will push the changes to the remote repository feature branc
 dprintf "git push -u origin feature1"
 echo '*******Now we will create a Pull Request'
 dprintf "hub pull-request -b uat -h feature1"
-echo '*******Now we will go into GitHub and review and merge the Pull Request'
+dprintf "echo '*******Now we will go into GitHub and review and merge the Pull Request'"
+echo '*******Now we will pull the changes from the repo to deploy them to UAT'
 dprintf "mkdir ../../dfdemo_working"
 dprintf "cd ../../dfdemo_working"
 dprintf "git clone https://github.com/$gitusername/dfdemo"
 dprintf "cd dfdemo/myproject"
 dprintf "git pull origin uat"
 dprintf "sfdx force:source:deploy -p force-app/ -u $uat -w 10"
+echo '*******Now we will go into the UAT sandbox to do final verification'
 dprintf "sfdx force:org:open -u $uat"
 dprintf "hub pull-request -b master -h uat"
