6587098e087d27223af835ebe80d1682ee419a61
kfidelak94 <31737796+kfidelak94@users.noreply.github.com>
2019-11-14 14:09:07 -0700

    added DreamSB1

--- auth.sh
+++ auth.sh
@@ -4,3 +4,4 @@ source ./autopilot.sh
 dprintf "sfdx force:auth:web:login --setalias Prod --setdefaultdevhubusername"
 dprintf "sfdx force:auth:web:login --setalias DevSB --instanceurl https://test.salesforce.com"
 dprintf "sfdx force:auth:web:login --setalias UAT --instanceurl https://test.salesforce.com"
+dprintf "sfdx force:auth:web:login --setalias DreamSB1 --instanceurl https://test.salesforce.com"
